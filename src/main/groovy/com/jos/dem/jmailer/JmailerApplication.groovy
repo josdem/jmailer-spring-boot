a8fd4fd1af40a90214c44d088741553391c9d5ca
josdem <joseluis.delacruz@gmail.com>
2015-11-27 20:07:04 -0600

    Changing to groovy project

---
+++ src/main/groovy/com/jos/dem/jmailer/JmailerApplication.groovy
@@ -0,0 +1,13 @@
+package com.jos.dem.jmailer
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JmailerApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run JmailerApplication.class, args
+  }
+
+}
---
+++ src/test/groovy/com/jos/dem/jmailer/JmailerApplicationTests.groovy
@@ -0,0 +1,18 @@
+package com.jos.dem.jmailer
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JmailerApplication.class)
+@WebAppConfiguration
+class JmailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
