9e880b94cc03c4e7c7cb77bc4c6ea0b1a9a7af98
josdem <joseluis.delacruz@gmail.com>
2015-11-27 22:01:47 -0600

    Adding missing import

--- src/main/groovy/com/jos/dem/jmailer/service/Receiver.groovy
+++ src/main/groovy/com/jos/dem/jmailer/service/Receiver.groovy
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.jms.annotation.JmsListener
 import org.springframework.stereotype.Component
+import org.springframework.util.FileSystemUtils
 
 @Component
 public class Receiver {
