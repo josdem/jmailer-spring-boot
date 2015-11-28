7e4bff14243bf343dc046140a66688cb79982b82
josdem <joseluis.delacruz@gmail.com>
2015-11-27 23:22:45 -0600

    Implementing send email

--- src/main/groovy/com/jos/dem/jmailer/messengine/JmsMessageListener.groovy
+++ src/main/groovy/com/jos/dem/jmailer/messengine/JmsMessageListener.groovy
@@ -14,12 +14,16 @@ import org.apache.commons.logging.LogFactory
 @Component
 class JmsMessageListener {
 
+  @Autowired
+  NotificationService notificationService
+
   Log log = LogFactory.getLog(this.class)
 
   @JmsListener(destination = "destination", containerFactory = "myJmsContainerFactory")
   public void receiveMessage(Message message) {
     Object command =  ((ObjectMessage) message).getObject()
     log.info "Received ${command.dump()}"
+    notificationService.sendNotification(command)
   }
 
 }
