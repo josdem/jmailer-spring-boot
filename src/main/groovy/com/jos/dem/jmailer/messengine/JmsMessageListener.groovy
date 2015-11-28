34b97435017a2db611c46cda3fee2d8067643962
josdem <joseluis.delacruz@gmail.com>
2015-11-27 22:40:29 -0600

    Leaving application alive

--- src/main/groovy/com/jos/dem/jmailer/messengine/JmsMessageListener.groovy
+++ src/main/groovy/com/jos/dem/jmailer/messengine/JmsMessageListener.groovy
@@ -1,7 +1,6 @@
 package com.jos.dem.jmailer.messengine
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.jms.annotation.JmsListener
 import org.springframework.stereotype.Component
 import org.springframework.util.FileSystemUtils
@@ -9,14 +8,9 @@ import org.springframework.util.FileSystemUtils
 @Component
 class JmsMessageListener {
 
-  @Autowired
-  ConfigurableApplicationContext context
-
   @JmsListener(destination = "mailbox-destination", containerFactory = "myJmsContainerFactory")
   public void receiveMessage(String message) {
     System.out.println("Received <" + message + ">")
-    context.close()
-    FileSystemUtils.deleteRecursively(new File("activemq-data"))
   }
 
 }
