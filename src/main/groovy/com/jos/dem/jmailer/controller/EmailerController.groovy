b13b8e02e95af654d35d1810801d222417805c05
josdem <joseluis.delacruz@gmail.com>
2015-11-27 20:38:04 -0600

    Throwing an emailer exception from service

--- src/main/groovy/com/jos/dem/jmailer/controller/EmailerController.groovy
+++ src/main/groovy/com/jos/dem/jmailer/controller/EmailerController.groovy
@@ -2,13 +2,19 @@ package com.jos.dem.jmailer.controller
 
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.beans.factory.annotation.Autowired
+
+import com.jos.dem.jmailer.service.EmailerService
 
 @RestController
-class SimpleController {
+class EmailerController {
+
+  @Autowired
+  EmailerService emailerService
 
   @RequestMapping("/")
   String index() {
-    "Greetings from Spring Boot!"
+    emailerService.sendEmail()
   }
 
 }
