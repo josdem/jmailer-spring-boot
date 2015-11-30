7f5dfc2a6927f731d57708b2294e1629b779e5dd
josdem <joseluis.delacruz@gmail.com>
2015-11-29 18:33:10 -0600

    Setting template and subject from properties

--- src/main/groovy/com/jos/dem/jmailer/constants/ApplicationConstants.groovy
+++
@@ -1,6 +0,0 @@
-package com.jos.dem.jmailer.constant
-
-interface ApplicationConstants {
-  final String SENDER = 'email.sender'
-  final String WHITE_LIST = 'white.list'
-}
--- src/main/groovy/com/jos/dem/jmailer/service/impl/NotificationServiceImpl.groovy
+++ src/main/groovy/com/jos/dem/jmailer/service/impl/NotificationServiceImpl.groovy
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service
 import com.jos.dem.jmailer.integration.MailService
 import com.jos.dem.jmailer.service.NotificationService
 import com.jos.dem.jmailer.command.MessageCommand
-import com.jos.dem.jmailer.constant.ApplicationConstants
+import org.springframework.beans.factory.annotation.Value
 
 import org.apache.commons.logging.Log
 import org.apache.commons.logging.LogFactory
@@ -18,24 +18,17 @@ class NotificationServiceImpl implements NotificationService {
   @Autowired
   MailService mailService
 
+  @Value('${email.template}')
+  String template
+  @Value('${email.subject}')
+  String subject
+
   private Log log = LogFactory.getLog(getClass())
 
   @Override
   void sendNotification(MessageCommand messageCommand) {
-    def (subject, templateName) = obtainSubjectAndResourceToSendNotification(messageCommand)
-      def data = [email:messageCommand.email, subject:subject, templateName:templateName]
-      mailService.sendMailWithTemplate(data, messageCommand.properties, data.templateName)
-  }
-
-  def obtainSubjectAndResourceToSendNotification(MessageCommand messageCommand){
-    String templateKey = "${messageCommand.type.toString()}_PATH"
-    String subjectKey = "${messageCommand.type.toString()}_SUBJECT"
-
-    String templateName = 'message.ftl'
-    String subject = 'hello'
-
-    log.info("Sending email with subject: " + subject)
-    [subject, templateName]
+     def data = [email:messageCommand.email, subject:subject]
+     mailService.sendMailWithTemplate(data, messageCommand.properties, template)
   }
 
 }
