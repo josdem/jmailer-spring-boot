c8e6362dc04ace886b7dec2c5ec9ff26d96b15ac
josdem <joseluis.delacruz@gmail.com>
2015-12-02 22:46:55 -0600

    Deleting unused WebAppConfiguration annotation

--- src/test/groovy/com/jos/dem/jmailer/JmailerApplicationTests.groovy
+++ src/test/groovy/com/jos/dem/jmailer/JmailerApplicationTests.groovy
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals
 
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,14 +11,13 @@ import org.springframework.mail.javamail.JavaMailSenderImpl
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = JmailerApplication.class)
-@WebAppConfiguration
 class JmailerApplicationTests {
 
   @Autowired
   JavaMailSenderImpl javaMailSender
 
 	@Test
-	void contextLoads() {
+	void shouldLoadContext() {
     assertEquals('smtp.gmail.com', javaMailSender.host)
     assertEquals(587, javaMailSender.port)
 	}
