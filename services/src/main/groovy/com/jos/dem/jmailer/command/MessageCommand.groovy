99513edd165ae2c11bd5f43ae13bb828a6c91b92
josdem <joseluis.delacruz@gmail.com>
2016-07-15 18:54:02 -0500

    Adding swagger api model properties

--- services/src/main/groovy/com/jos/dem/jmailer/command/MessageCommand.groovy
+++ services/src/main/groovy/com/jos/dem/jmailer/command/MessageCommand.groovy
@@ -16,14 +16,24 @@
 
 package com.jos.dem.jmailer.command
 
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+
 import com.jos.dem.jmailer.enums.MessageType
 
+@ApiModel(value="MessageCommand", description="Generic command to send emails")
 class MessageCommand implements Command {
+  @ApiModelProperty(value = "Any valid email", allowableValues = "email@domain")
   String email
+  @ApiModelProperty(value = "Email body", allowableValues = "text")
   String message
+  @ApiModelProperty(value = "name", allowableValues = "Email receipt name")
   String name
+  @ApiModelProperty(value = "Any valid email", allowableValues = "email@domain")
   String emailContact
+  @ApiModelProperty(value = "source", allowableValues = "josdem.io,jmailer,gauthenticator")
   String source
+  @ApiModelProperty(value = "type", allowableValues = "REGISTER")
   MessageType type
 }
 
