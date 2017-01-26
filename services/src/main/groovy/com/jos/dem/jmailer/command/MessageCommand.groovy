8ad40d7e77500e63b8533558cc957253c1236fcf
josdem <joseluis.delacruz@gmail.com>
2017-01-25 21:47:10 -0600

    #3 Deleting unused fields

--- services/src/main/groovy/com/jos/dem/jmailer/command/MessageCommand.groovy
+++ services/src/main/groovy/com/jos/dem/jmailer/command/MessageCommand.groovy
@@ -1,41 +1,28 @@
 /*
-  Copyright 2016 José Luis De la Cruz Morales joseluis.delacruz@gmail.com
+Copyright 2016 José Luis De la Cruz Morales joseluis.delacruz@gmail.com
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-  http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 
 package com.jos.dem.jmailer.command
 
-import com.jos.dem.jmailer.enums.MessageType
-
-import io.swagger.annotations.ApiModel
-import io.swagger.annotations.ApiModelProperty
-import org.hibernate.validator.constraints.Email
-import javax.validation.constraints.NotNull
-import org.hibernate.validator.constraints.NotEmpty
-
-@ApiModel(value="MessageCommand", description="Generic command to send emails")
-class MessageCommand implements Command {
-  @ApiModelProperty(value = "Any valid emial", allowableValues = "email@domain")
-  @Email
-  @NotEmpty
-  @NotNull
-  String email
-  @ApiModelProperty(value = "Email body", allowableValues = "text")
-  @NotNull
-  String message
-  @ApiModelProperty(value = "User's name", allowableValues = "text")
+class MessgeCommand implements Command {
   String name
-  MessageType type
+  String email
+  String url
+  String message
+  String source
+  String redirect
+  String emailContact
 }
 
