fd42e80301d404980d231c0f7bb668ee49467fe2
josdem <joseluis.delacruz@gmail.com>
2017-08-28 17:55:19 -0500

    #10 Fixing some imports

--- web/src/main/groovy/com/jos/dem/jmailer/controller/EmailerController.groovy
+++ web/src/main/groovy/com/jos/dem/jmailer/controller/EmailerController.groovy
@@ -16,29 +16,31 @@ limitations under the License.
 
 package com.jos.dem.jmailer.controller
 
-import org.springframework.beans.factory.annotation.Autowired
+import static org.springframework.web.bind.annotation.RequestMethod.GET
+import static org.springframework.web.bind.annotation.RequestMethod.POST
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.servlet.ModelAndView
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.servlet.ModelAndView
-import org.springframework.http.ResponseEntity
-import org.springframework.http.HttpStatus
+
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiImplicitParam
 import io.swagger.annotations.ApiImplicitParams
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET
-import static org.springframework.web.bind.annotation.RequestMethod.POST
 
 import com.jos.dem.jmailer.service.EmailerService
 import com.jos.dem.jmailer.command.MessageCommand
 import com.jos.dem.jmailer.exception.BusinessException
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @Api(description="Knows how to send emails")
 @RequestMapping("/emailer/*")
