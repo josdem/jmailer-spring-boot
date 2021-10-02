fad8e8d1e38cad69fc716d640e06425a9c216e01
josdem <joseluis.delacruz@gmail.com>
2021-10-02 10:29:52 -0500

    #18 Moving config module to Java

--- web/src/main/java/com/jos/dem/jmailer/config/SwaggerConfig.java
+++ web/src/main/java/com/jos/dem/jmailer/config/SwaggerConfig.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 José Luis De la Cruz Morales joseluis.delacruz@gmail.com
+Copyright 2021 Jose Morales joseluis.delacruz@gmail.com
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,26 +14,26 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package com.jos.dem.jmailer.config
+package com.jos.dem.jmailer.config;
 
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.spi.DocumentationType
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.swagger2.annotations.EnableSwagger2
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-class SwaggerConfig {
+public class SwaggerConfig {
   @Bean
   Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
     .select()
     .apis(RequestHandlerSelectors.any())
     .paths(PathSelectors.any())
-    .build()
+    .build();
   }
 }
 
