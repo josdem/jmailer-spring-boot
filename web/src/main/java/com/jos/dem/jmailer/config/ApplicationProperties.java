bd17201a885ab8255b3da26d9f7a474bb4fe8772
josdem <joseluis.delacruz@gmail.com>
2022-05-07 18:56:32 -0400

    #20 Adding application properties with Swagger values

---
+++ web/src/main/java/com/jos/dem/jmailer/config/ApplicationProperties.java
@@ -0,0 +1,18 @@
+package com.jos.dem.jmailer.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("api")
+public class ApplicationProperties {
+    private String version;
+    private String title;
+    private String basePackage;
+    private String description;
+    private String terms;
+}
--- web/src/main/java/com/jos/dem/jmailer/config/SwaggerConfig.java
+++ web/src/main/java/com/jos/dem/jmailer/config/SwaggerConfig.java
@@ -16,24 +16,44 @@ limitations under the License.
 
 package com.jos.dem.jmailer.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Set;
+
 @Configuration
 @EnableSwagger2
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+  private final ApplicationProperties applicationProperties;
+
   @Bean
-  Docket api() {
+  public Docket createDocket() {
     return new Docket(DocumentationType.SWAGGER_2)
-    .select()
-    .apis(RequestHandlerSelectors.any())
-    .paths(PathSelectors.any())
-    .build();
+            .useDefaultResponseMessages(false)
+            .protocols(Set.of("https", "http"))
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage(applicationProperties.getBasePackage()))
+            .build();
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+            .title(applicationProperties.getTitle())
+            .description(applicationProperties.getDescription())
+            .termsOfServiceUrl(applicationProperties.getTerms())
+            .version(applicationProperties.getVersion())
+            .build();
   }
 }
 
