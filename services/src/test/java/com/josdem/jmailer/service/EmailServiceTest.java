b0f7d6ff33fc7ce77d2c64001c15841ea1b9f4dc
josdem <joseluis.delacruz@gmail.com>
2024-12-26 09:05:18 -0600

    #29 Renaming packages

---
+++ services/src/test/java/com/josdem/jmailer/service/EmailServiceTest.java
@@ -0,0 +1,55 @@
+/*
+  Copyright 2023 Jose Morales joseluis.delacruz@gmail.com
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.josdem.jmailer.service;
+
+import com.josdem.jmailer.command.Command;
+import com.josdem.jmailer.service.impl.EmailerServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@Slf4j
+class EmailServiceTest {
+
+  private EmailerService emailerService;
+
+  @Mock private MessageService messageService;
+  @Mock private EmailValidatorService emailValidatorService;
+
+  @BeforeEach
+  void setup() {
+    MockitoAnnotations.openMocks(this);
+    emailerService = new EmailerServiceImpl(messageService, emailValidatorService);
+  }
+
+  @Test
+  @DisplayName("send a message")
+  void shouldSendMessage(TestInfo testInfo) {
+    log.info("Running {}", testInfo.getDisplayName());
+    Command command = mock(Command.class);
+    emailerService.sendEmail(command);
+    verify(messageService).message(command);
+    verify(emailValidatorService).validate(command);
+  }
+}
---
+++ web/src/main/java/com/josdem/jmailer/controller/EmailerController.java
@@ -0,0 +1,89 @@
+/*
+  Copyright 2024 Jose Morales contact@josdem.io
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+package com.josdem.jmailer.controller;
+
+import com.josdem.jmailer.command.MessageCommand;
+import com.josdem.jmailer.exception.BusinessException;
+import com.josdem.jmailer.service.EmailerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Slf4j
+@Tag(name = "email", description = "Knows how to send emails")
+@RequestMapping("/emailer/*")
+@RestController
+@RequiredArgsConstructor
+public class EmailerController {
+
+    private final EmailerService emailerService;
+
+    @Value("${token}")
+    private String token;
+
+    @Value("${email.redirect}")
+    private String redirectUrl;
+
+    @Operation(summary = "Send an email with JSON")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "User created"),
+                    @ApiResponse(responseCode = "400", description = "Bad request"),
+                    @ApiResponse(responseCode = "500", description = "Something went wrong")
+            })
+    @RequestMapping(method = POST, value = "/message", consumes = "application/json")
+    public ResponseEntity<String> message(@RequestBody MessageCommand command) {
+        log.info("Request contact email: {}", command.getEmail());
+        if (!token.equals(command.getToken())) {
+            return new ResponseEntity<String>("FORBIDDEN", HttpStatus.FORBIDDEN);
+        }
+        emailerService.sendEmail(command);
+        return new ResponseEntity<String>("OK", HttpStatus.OK);
+    }
+
+    @RequestMapping(method = POST, value = "/form", consumes = "application/x-www-form-urlencoded")
+    public ModelAndView form(@Valid MessageCommand command) {
+        log.info("Request message from: {}", command);
+        if (!token.equals(command.getToken())) {
+            log.info("Invalid user's token");
+            return new ModelAndView("redirect:/contact");
+        }
+        emailerService.sendEmail(command);
+        return new ModelAndView("redirect:" + command.getRedirect());
+    }
+
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Unauthorized")
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<String> handleException(BusinessException be) {
+        return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
+    }
+}
