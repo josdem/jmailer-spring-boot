6cd73e04a5d9f42a6cfbad613fd2892054edd1ea
josdem <joseluis.delacruz@gmail.com>
2025-04-09 05:31:25 -0600

    #163 Adding business exception test

--- services/src/test/java/com/josdem/jmailer/config/TemplateConfigTest.java
+++ services/src/test/java/com/josdem/jmailer/config/TemplateConfigTest.java
@@ -16,6 +16,10 @@
 
 package com.josdem.jmailer.config;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -25,60 +29,53 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mail.javamail.JavaMailSender;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
 @Slf4j
 class TemplateConfigTest {
 
-    private TemplateConfig templateConfig;
+  private TemplateConfig templateConfig;
 
-    @Mock
-    private JavaMailSender defaultMailSender;
+  @Mock private JavaMailSender defaultMailSender;
 
-    @Mock
-    private JavaMailSender vetlogMailSender;
+  @Mock private JavaMailSender vetlogMailSender;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        templateConfig = new TemplateConfig(defaultMailSender, vetlogMailSender);
-    }
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    templateConfig = new TemplateConfig(defaultMailSender, vetlogMailSender);
+  }
 
-    @Test
-    @DisplayName("getting a template strategies")
-    void shouldGetTemplateStrategies(TestInfo testInfo) {
-        log.info(testInfo.getDisplayName());
-        var templateStrategy = templateConfig.templateStrategy();
-        assertNotNull(templateStrategy);
-        assertEquals(8, templateStrategy.size());
-        assertTrue(templateStrategy.containsKey("welcome.ftl"));
-        assertTrue(templateStrategy.containsKey("welcome_es.ftl"));
-        assertTrue(templateStrategy.containsKey("adoption.ftl"));
-        assertTrue(templateStrategy.containsKey("adoption_es.ftl"));
-        assertTrue(templateStrategy.containsKey("forgotPassword.ftl"));
-        assertTrue(templateStrategy.containsKey("forgotPassword_es.ftl"));
-        assertTrue(templateStrategy.containsKey("message.ftl"));
-        assertTrue(templateStrategy.containsKey("register.ftl"));
-    }
+  @Test
+  @DisplayName("getting a template strategies")
+  void shouldGetTemplateStrategies(TestInfo testInfo) {
+    log.info(testInfo.getDisplayName());
+    var templateStrategy = templateConfig.templateStrategy();
+    assertNotNull(templateStrategy);
+    assertEquals(8, templateStrategy.size());
+    assertTrue(templateStrategy.containsKey("welcome.ftl"));
+    assertTrue(templateStrategy.containsKey("welcome_es.ftl"));
+    assertTrue(templateStrategy.containsKey("adoption.ftl"));
+    assertTrue(templateStrategy.containsKey("adoption_es.ftl"));
+    assertTrue(templateStrategy.containsKey("forgotPassword.ftl"));
+    assertTrue(templateStrategy.containsKey("forgotPassword_es.ftl"));
+    assertTrue(templateStrategy.containsKey("message.ftl"));
+    assertTrue(templateStrategy.containsKey("register.ftl"));
+  }
 
-    @Test
-    @DisplayName("getting a default client")
-    void shouldGetDefaultClient(TestInfo testInfo) {
-        log.info(testInfo.getDisplayName());
-        var templateStrategy = templateConfig.templateStrategy();
-        var defaultClient = templateStrategy.get("message.ftl");
-        assertNotNull(defaultClient);
-    }
+  @Test
+  @DisplayName("getting a default client")
+  void shouldGetDefaultClient(TestInfo testInfo) {
+    log.info(testInfo.getDisplayName());
+    var templateStrategy = templateConfig.templateStrategy();
+    var defaultClient = templateStrategy.get("message.ftl");
+    assertNotNull(defaultClient);
+  }
 
-    @Test
-    @DisplayName("getting a vetlog client")
-    void shouldGetVetlogClient(TestInfo testInfo) {
-        log.info(testInfo.getDisplayName());
-        var templateStrategy = templateConfig.templateStrategy();
-        var vetlogClient = templateStrategy.get("welcome.ftl");
-        assertNotNull(vetlogClient);
-    }
+  @Test
+  @DisplayName("getting a vetlog client")
+  void shouldGetVetlogClient(TestInfo testInfo) {
+    log.info(testInfo.getDisplayName());
+    var templateStrategy = templateConfig.templateStrategy();
+    var vetlogClient = templateStrategy.get("welcome.ftl");
+    assertNotNull(vetlogClient);
+  }
 }
