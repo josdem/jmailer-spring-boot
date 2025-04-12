d6d18a058e3f7cde991d6ec145c1699dc6b0cf6a
josdem <joseluis.delacruz@gmail.com>
2025-04-12 07:12:07 -0600

    #113 Completing test

--- services/src/test/java/com/josdem/jmailer/config/TemplateConfigTest.kt
+++ services/src/test/java/com/josdem/jmailer/config/TemplateConfigTest.kt
@@ -1,3 +1,19 @@
+/*
+  Copyright 2025 Jose Morales contact@josdem.io
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 package com.josdem.jmailer.config
 
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -50,5 +66,13 @@ internal class TemplateConfigTest {
         val templateStrategy = templateConfig.templateStrategy()
         val defaultClient = templateStrategy["message.ftl"]
         assertNotNull(defaultClient)
+    }
+
+    @Test
+    fun `getting vetlog client`(testInfo: TestInfo) {
+        log.info(testInfo.displayName)
+        val templateStrategy = templateConfig.templateStrategy()
+        val vetlogClient = templateStrategy["welcome.ftl"]
+        assertNotNull(vetlogClient)
     }
 }
